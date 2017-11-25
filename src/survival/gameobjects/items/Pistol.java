60008b84dcd6cf6264eb8f26e5b32961b9b53d9c
Amos <amos.george0@gmail.com>
2017-11-25 21:56:31 +0100

    add pistol

---
+++ src/survival/gameobjects/items/Pistol.java
@@ -0,0 +1,17 @@
+package survival.gameobjects.items;
+
+import survival.gameobjects.gameplay.Player;
+
+public class Pistol {
+	
+	private Player player;
+	private float damage;
+	private float range;
+	private int ammo;
+	
+	public Pistol(float damage, float range, int ammo) {
+		this.damage = damage;
+		this.range = range;
+		this.ammo = ammo;
+	}
+}
