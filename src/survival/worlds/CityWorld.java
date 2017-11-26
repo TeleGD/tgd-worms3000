5e31a7d3c7945ee18a6a6800030f955ec300a00d
MehdiTheMedoc <mehdi.akkari@telecomnancy.net>
2017-11-26 05:20:39 +0100

    ajout de routes

--- src/survival/worlds/CityWorld.java
+++ src/survival/worlds/CityWorld.java
@@ -26,26 +26,38 @@ public class CityWorld extends World{
 		batiments[0] = new Image(SurvivalMain.DIRECTORY_IMAGES + "/deccors/Batiments1.png");
 		batiments[1] = new Image(SurvivalMain.DIRECTORY_IMAGES + "/deccors/batiment2.png");
 		
-		Image road = new Image(SurvivalMain.DIRECTORY_IMAGES + "/deccors/road0.png");
+		Image road0 = new Image(SurvivalMain.DIRECTORY_IMAGES + "/deccors/road0.png");
+		Image road1 = new Image(SurvivalMain.DIRECTORY_IMAGES + "/deccors/road1.png");
 		
 		addBackground(new RepeatBackground(new Image(SurvivalMain.DIRECTORY_IMAGES + "backgrounds/concrete0.jpg"), new Vector2(0,0)));
 		
-		for(int i=0; i<25; i++)
-		{
-			addGameObject(new MapObject(road, new Vector2(i*256,75), false));
-		}
+		addGameObject(new MapObject(new Image(SurvivalMain.DIRECTORY_IMAGES + "/deccors/road2.png"), new Vector2(-256,75), false));
 		
 		for(int i=0; i<25; i++)
 		{
+			addGameObject(new MapObject(road0, new Vector2(i*256,75), false));
+			addGameObject(new MapObject(road1, new Vector2(-256,75+256+i*256), false));
+			
+			addGameObject(new MapObject(road0, new Vector2(-i*256-256-256,75), false));
+			addGameObject(new MapObject(road1, new Vector2(-256,75-256-i*256), false));
+		}
+		
+		
+		for(int i=0; i<25; i++)
+		{
+			addGameObject(new MapObject(batiments[(int)(Math.random()+0.5f)], new Vector2(-512,-25*170+i*170)));
+			addGameObject(new MapObject(batiments[(int)(Math.random()+0.5f)], new Vector2(0,-25*170+i*170)));
+			
 			addGameObject(new MapObject(batiments[(int)(Math.random()+0.5f)], new Vector2(i*240,-200)));
+			addGameObject(new MapObject(batiments[(int)(Math.random()+0.5f)], new Vector2(-i*240-512,-200)));
+			addGameObject(new MapObject(batiments[(int)(Math.random()+0.5f)], new Vector2(i*240,300)));
+			addGameObject(new MapObject(batiments[(int)(Math.random()+0.5f)], new Vector2(-i*240-512,300)));
+			
+			addGameObject(new MapObject(batiments[(int)(Math.random()+0.5f)], new Vector2(-512,300+300+i*170)));
+			addGameObject(new MapObject(batiments[(int)(Math.random()+0.5f)], new Vector2(0,300+300+i*170)));
 		}
 		
 		addGameObject(new MapObject(new Image(SurvivalMain.DIRECTORY_IMAGES + "/deccors/PanneauStop.png"), new Vector2(-140,-100), false));
-		
-		for(int i=0; i<25; i++)
-		{
-			addGameObject(new MapObject(batiments[(int)(Math.random()+0.5f)], new Vector2(i*240,300)));
-		}
 	}
 
 
@@ -62,11 +74,17 @@ public class CityWorld extends World{
 		
 		World.activePlayer = new TestObject(new Image(SurvivalMain.DIRECTORY_IMAGES + "chara.png"));
 		addGameObject(World.activePlayer);
-		addGameObject(new Infected(new Image(SurvivalMain.DIRECTORY_IMAGES + "infected.png"), new Vector2(-400,-400),1f,2.5f));
-		addGameObject(new Infected(new Image(SurvivalMain.DIRECTORY_IMAGES + "infected.png"), new Vector2(-460,-400),1f,2.5f));
-		addGameObject(new Infected(new Image(SurvivalMain.DIRECTORY_IMAGES + "infected.png"), new Vector2(-520,-400),1f,2.5f));
-		addGameObject(new Infected(new Image(SurvivalMain.DIRECTORY_IMAGES + "infected.png"), new Vector2(-580,-400),1f,2.5f));
 		
+		Image infect = new Image(SurvivalMain.DIRECTORY_IMAGES + "infected.png");
+		
+		for(int i=3; i<25; i+=2)
+		{
+			addGameObject(new Infected(infect, new Vector2(i*256,0),1f,2.5f));
+			addGameObject(new Infected(infect, new Vector2(-i*256,0),1f,2.5f));
+			
+			addGameObject(new Infected(infect, new Vector2(-256,i*256),1f,2.5f));
+			addGameObject(new Infected(infect, new Vector2(-256,-i*256),1f,2.5f));
+		}
 		
 		
 		addUiGameObject(new Compteur(30,100, new Vector2(500,650), new Vector2(10,11),new Image(SurvivalMain.DIRECTORY_IMAGES + "ui/barre.png"), new Image(SurvivalMain.DIRECTORY_IMAGES + "ui/HungryBar.png")));
