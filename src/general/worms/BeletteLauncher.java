48699fb31b5be43bb017e66e0780d01918b70041
remim-j <remi.marie-jeanne@telecomnancy.eu>
2017-11-26 04:34:55 +0100

    WeaselPowAAAA

--- src/general/worms/BeletteLauncher.java
+++ src/general/worms/BeletteLauncher.java
@@ -3,15 +3,13 @@ package general.worms;
 public class BeletteLauncher extends Weapon {
 
 	public BeletteLauncher(){
-		super(14,10,0,750,130,-1);
+		super(99,500000,0,200,1,-1);
 	}
 	@Override
 	protected void fireOneShot(int x, int y, float alpha) {
-		for (int i=0; i < 7; i++) {
-			float deltaAlpha = (float) ((Math.random() * 360 - 3.5) * (2*Math.PI) / 360.);
-			Projectile proj = new Projectile(x, y, alpha + deltaAlpha, this.firepower, this.weight, this);
-			World.addProjectile(proj);
-		}
+		ProjectileSinusoidal proj = new ProjectileSinusoidal(x, y, alpha, this.firepower, this.weight, this);
+		World.addProjectile(proj);
+
 	}
 
 }
