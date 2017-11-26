ce8104910beaf5e79e883c82bd09313ea264a2e6
Derpy <moran.leterrier@telecomnancy.net>
2017-11-26 05:23:04 +0100

    PathUtils + Remix images path

---
+++ src/general/worms/PathUtils.java
@@ -0,0 +1,63 @@
+package general.worms;
+
+public class PathUtils {
+
+	private static String Weapons = "images/Worms/Weapons/";
+	private static String UI = "images/Worms/UI/";
+	private static String Terrain = "images/Worms/Terrain/";
+	private static String Perso = "images/Worms/Perso/";
+	private static String OldTerrain = "images/Worms/OldTerrain/";
+	private static String OldPerso = "images/Worms/OldPerso/";
+	private static String Loot = "images/Worms/Loot/";
+	private static String Belette = "images/Worms/Belette/";
+	
+	public static String Bazooka = Weapons + "Bazooka.png";
+	public static String Bazooka_Bullet = Weapons + "Bazooka_Bullet.png";
+	public static String Shotgun = Weapons + "Shotgun.png";
+	public static String Sword = Weapons + "Sword.png";
+	public static String Uzy = Weapons + "Uzy.png";
+	
+	public static String HPBar = UI + "HPBar.png";
+	
+	public static String Dirt = Terrain + "Dirt.png";
+	public static String DirtMap_1 = Terrain + "DirtMap_1.png";
+	public static String DirtMap_2 = Terrain + "DirtMap_2.png";
+	public static String DirtTile = Terrain + "DirtTile.png";
+	public static String Grass = Terrain + "Grass.png";
+	
+	private static String Idle = Perso + "Idle/";
+	private static String Jump = Perso + "Jump/";
+	private static String Walk = Perso + "Walk/";
+	
+	public static String PersoLeftBlu = Idle + "PersoLeftBlu.png";
+	public static String PersoLeftRed = Idle + "PersoLeftRed.png";
+	public static String PersoRightBlu = Idle + "PersoRightBlu.png";
+	public static String PersoRightRed = Idle + "PersoRightRed.png";
+	
+	public static String PersoLeftBluJump = Jump + "PersoLeftBluJump.png";
+	public static String PersoLeftRedJump = Jump + "PersoLeftRedJump.png";
+	public static String PersoRightBluJump = Jump + "PersoRightBluJump.png";
+	public static String PersoRightRedJump = Jump + "PersoRightRedJump.png";
+	
+	public static String PersoLeftBluWalk = Walk + "PersoLeftBluWalk.png";
+	public static String PersoLeftRedWalk = Walk + "PersoLeftRedWalk.png";
+	public static String PersoRightBluWalk = Walk + "PersoRightBluWalk.png";
+	public static String PersoRightRedWalk = Walk + "PersoRightRedWalk.png";
+
+	public static String DiagD = OldTerrain + "DiagD.png";
+	public static String DiagG = OldTerrain + "DiagG.png";
+	public static String DiagMiniD = OldTerrain + "DiagMiniD.png";
+	public static String DiagMiniG = OldTerrain + "DiagMiniG.png";
+	public static String Horizontal = OldTerrain + "Horizontal.png";
+	
+	public static String Blu = OldPerso + "Blu.png";
+	public static String Red = OldPerso + "Red.png";
+	
+	public static String AmmoCrate = Loot + "AmmoCrate.png";
+	public static String HealthCrate = Loot + "HealthCrate.png";
+	public static String Parachute = Loot + "Parachute.png";
+	
+	public static String Belette_1 = Belette + "Belette_1.png";
+	public static String Belette_2 = Loot + "Belette_2.png";
+	
+}
